71aa04b8f332b6da78b45c82ec4bb2ed0ba179d6
wannabefsd <91594754+wannabefsd@users.noreply.github.com>
2021-10-25 12:18:35 +0400

    Update script.js
    corrected rate variable syntax

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function compute()
 {
         p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate/100;
     var year = new Date().getFullYear()+parseInt(years);
