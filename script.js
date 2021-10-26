6dd8a60e3a5bdc090acfa21776aa3052b779ca00
wannabefsd <91594754+wannabefsd@users.noreply.github.com>
2021-10-26 08:51:56 +0400

    edit slider function

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function compute()
     {
         var rateval = document.getElementById("rate").value;
         document.getElementById("rate_val").innerText=rateval;
-    
+        output.innerText = this.value
 }
         
