7f5384a7f8902d625f8777fdb72a8827d7249398
wannabefsd <91594754+wannabefsd@users.noreply.github.com>
2021-10-26 09:10:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 function compute()
 {
-        p = document.getElementById("principal").value;
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate/100;
-    var year = new Date().getFullYear()+parseInt(years);
-    
-    function updateRate()
-    {
-        var rateval = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText=rateval;
-    slider.oninput = function()
-  output.innerHTML = this.value;
+    var t = document.getElementById("principal").value;
+    var p = Number(t)
+    if (p<=0) {
+    	alert("Please enter positive Principal");
+		principal.focus();
+		return false;
+    }
+    var  r = document.getElementById("rate").value / 10;
+    if (r==0) {
+    	r = 10;
+    }
+    var y = document.getElementById("years").value;
+    if (y==0) { y = 1;}
+
+    var d = new Date();
+	var fut = Number(d.getFullYear());	
+	fut += Number(y)
+    rslt =  (p * y * r / 100);
+    ans = '<p>If you deposit <span class="yel">' + p  + '</span></p>'
+    ans += '<p>an interest rate of <span class="yel">' + r + '%. </span></p>';
+    ans = ans + '<p>You will receive an amount of <span class="yel">' + rslt + ',</span> </p>';
+    ans = ans + '<p>in the year <span class="yel">' + fut + '</span></p>'
+    document.getElementById("result").innerHTML = ans;
+    //alert(rslt)
+    return ans;
 }
-        
